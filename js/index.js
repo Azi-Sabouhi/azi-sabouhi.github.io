79dff2fde0cf4a321475e0eaddfbc325a4ff557f
Azi-Sabouhi <122724433+Azi-Sabouhi@users.noreply.github.com>
2023-07-18 17:09:38 +0330

    photo Updated

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// To local Site+++++++++++++++++++++++++++++++++++++++
+const CloseBTN = document.querySelector("#closeLocal");
+const LocalBox = document.querySelector("#LocalWebsite");
+const Dumy = document.querySelector("#bee");
+CloseBTN.addEventListener("click", function () {
+    LocalBox.classList.add("localToZiro");
+    Dumy.classList.add("displayHide");
+})
+// ----------------------------------------------------
+
