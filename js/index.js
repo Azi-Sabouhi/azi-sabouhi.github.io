fb72701f88df83800b50eb50eb3490fc183d399c
Azi-Sabouhi <122724433+Azi-Sabouhi@users.noreply.github.com>
2023-07-21 17:35:37 +0330

    update on slideshow and others

--- js/index.js
+++ js/index.js
@@ -8,3 +8,70 @@ CloseBTN.addEventListener("click", function () {
 })
 // ----------------------------------------------------
 
+// setting ++++++++++++++++++++++++++++++++++
+const Setting = document.querySelector(".setting");
+const SettingBox = document.querySelector(".settingBox");
+const Close = document.querySelector(".CloseInSettingBox");
+
+Setting.addEventListener("click", function () {
+    SettingBox.classList.toggle("settingToZiro");
+})
+Close.addEventListener("click", function () {
+    SettingBox.classList.remove("settingToZiro");
+})
+
+
+// ----------------------------------------------------------------
+
+// switch-button ++++++++++++++++++++++++++++++++
+const Button1 = document.querySelectorAll(".switch-button");
+const Switch1 = document.querySelectorAll(".switchOn");
+
+Switch1[0].addEventListener("click", function () {
+    Button1[0].classList.toggle("switch-button-position");
+    // Button1[1].classList.toggle("switch-button-position");
+    // Button1[2].classList.toggle("switch-button-position");
+})
+Switch1[1].addEventListener("click", function () {
+    Button1[1].classList.toggle("switch-button-position");
+})
+Switch1[2].addEventListener("click", function () {
+    Button1[2].classList.toggle("switch-button-position");
+})
+
+
+
+// ----------------------------------------------Slider-----------------
+const Back = document.querySelector(".left");
+const Next = document.querySelector(".right");
+
+Next.addEventListener("click", function () {
+    let screenWidth = document.documentElement.clientWidth;
+    let firstImage = document.querySelector(".size2");
+    firstImage.style.marginLeft = `${screenWidth}px`;
+    setTimeout(function () {
+        let firstImageSource = firstImage.getAttribute("src");
+        firstImage.remove();
+        // let lastImage = document.createElement("img");
+        lastImage.setAttribute("src".firstImageSource);
+        Back.before(lastImage);
+    }, 500)
+});
+Back.addEventListener("click", function () {
+    let screenwidth = document.documentElement.clientWidth;
+    let lastImage = document.querySelectorAll(".size2")[document.querySelectorAll(".size2").lenght - 1];
+    let lastImageSource = lastImage.getAttribute("src");
+    lastImage.remove();
+    // let firstImage = document.createElement("img");
+    firstImage.setAttribute("src".lastImageSource);
+    firstImage.style.marginLeft = `${-screenwidth}px`;
+    document.querySelector("slider-container").prepend(firstImage);
+    setTimeout(function () {
+        firstImage.style.marginLeft = 0;
+    }, 100);
+});
+
+
+
+
+// 
