d134346817bc73b3ecaba61b02dd73af6625f54c
Azi-Sabouhi <122724433+Azi-Sabouhi@users.noreply.github.com>
2023-07-22 13:43:48 +0330

    update footer

--- js/index.js
+++ js/index.js
@@ -71,6 +71,123 @@ Back.addEventListener("click", function () {
     }, 100);
 });
 
+// footer 88888888888888888888888888888888888888888888  minifooter
+const DIVS = [document.getElementById("community"), document.getElementById("support"), document.getElementById("resource"), document.getElementById("companyInfo"), document.getElementById("more")];
+
+const PLUSES = [document.getElementById("fst"), document.getElementById("fst2"), document.getElementById("fst3"), document.getElementById("fst4"), document.getElementById("fst5")];
+
+const BRDS = [document.getElementById("title1"), document.getElementById("title2"), document.getElementById("title3"), document.getElementById("title4"), document.getElementById("title5")];
+
+const BTN = document.querySelectorAll("button");
+const DIV = document.getElementById("community");
+const PLUS = document.getElementById("p1");
+const BRD = document.getElementById("title1");
+
+
+const DIV2 = document.getElementById("support");
+const PLUS2 = document.getElementById("p2");
+const BRD2 = document.getElementById("title2");
+
+
+const DIV3 = document.getElementById("resource3");
+const PLUS3 = document.getElementById("p3");
+const BRD3 = document.getElementById("title3");
+
+
+const DIV4 = document.getElementById("companyInfo");
+const PLUS4 = document.getElementById("p4");
+const BRD4 = document.getElementById("title4");
+
+
+const DIV5 = document.getElementById("more");
+const PLUS5 = document.getElementById("p5");
+const BRD5 = document.getElementById("title5");
+
+const HEIGHT = document.getElementById("adjustft");
+
+
+function closeAll(x) {
+    for (let i = 0; i < 5; i++) {
+        if (i == x) {
+            continue;
+        }
+        DIVS[i].classList.remove("open-footer");
+        PLUSES[i].classList.remove("rotate");
+        BRDS[i].classList.remove("hideborder");
+    }
+
+}
+
+BTN[0].addEventListener("click", function () {
+
+    closeAll(0);
+
+    PLUS.classList.toggle("rotate");
+    DIV.classList.toggle("open-footer");
+    BRD.classList.toggle("hideborder");
+});
+
+BTN[1].addEventListener("click", function () {
+
+
+    closeAll(1);
+
+    PLUS2.classList.toggle("rotate");
+    DIV2.classList.toggle("open-footer");
+    BRD2.classList.toggle("hideborder");
+
+});
+
+BTN[2].addEventListener("click", function () {
+
+    closeAll(2);
+
+
+    PLUS3.classList.toggle("rotate");
+    DIV3.classList.toggle("open-footer");
+    BRD3.classList.toggle("hideborder");
+
+
+});
+
+
+BTN[3].addEventListener("click", function () {
+
+
+    closeAll(3);
+
+
+    PLUS4.classList.toggle("rotate");
+    DIV4.classList.toggle("open-footer");
+    BRD4.classList.toggle("hideborder");
+
+
+});
+BTN[4].addEventListener("click", function () {
+
+
+    closeAll(4);
+
+
+    PLUS4.classList.toggle("rotate");
+    DIV4.classList.toggle("open-footer");
+    BRD4.classList.toggle("hideborder");
+
+
+});
+BTN[5].addEventListener("click", function () {
+
+
+    closeAll(5);
+
+
+    PLUS4.classList.toggle("rotate");
+    DIV4.classList.toggle("open-footer");
+    BRD4.classList.toggle("hideborder");
+
+
+});
+
 
 
 
